a216c299d7c8fcb80c65cf836916836716549c48
Danish Jafri <danishjafri@outlook.com>
2022-04-24 22:00:32 +0100

    Added Student entity with Validation using Data Annotation.

---
+++ StudentAdmissions/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAdmissions.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        [MinLength(3)]
+        [MaxLength(20)] 
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        [MinLength(3, ErrorMessage = "Age cannot be less than 18")]
+        public int Age { get; set; }
+    }
+}
