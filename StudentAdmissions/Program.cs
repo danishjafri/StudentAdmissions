7f3b7daac0736f18a1a2020e4b1289c51f5bd171
Danish Jafri <danishjafri@outlook.com>
2022-04-24 22:06:32 +0100

    Added Student Web API template.

--- StudentAdmissions/Program.cs
+++ StudentAdmissions/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentContext") ?? throw new InvalidOperationException("Connection string 'StudentContext' not found.")));
 
 // Add services to the container.
 
